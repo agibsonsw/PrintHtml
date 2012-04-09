daa2bd35b699dcd562211da01e1408962ffc4e22
Andrew Gibson <agibsonsw@gmail.com>
2012-04-09 19:10:46 +0100

    Print selected text, line numbering, as options

--- PrintHtml.py
+++ PrintHtml.py
@@ -4,7 +4,7 @@ import re
 from os import path
 
 class PrintHtmlCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
+	def run(self, edit, numbers):		# numbers == True: output line numbers
 		self.colours = {}
 		path_packages = sublime.packages_path()
 		settings = sublime.load_settings('Preferences.sublime-settings')
@@ -20,13 +20,15 @@ class PrintHtmlCommand(sublime_plugin.TextCommand):
 		the_dict = doc.getElementsByTagName('dict')[0]
 		the_array = the_dict.getElementsByTagName('array')[0]
 		colour_settings = the_array.getElementsByTagName('dict')[0]
-		bground = ''; fground = ''
+		bground = ''; fground = ''; gfground = ''
 		for key_tag in colour_settings.getElementsByTagName('key'):
 			try:
 				if key_tag.firstChild.data.strip() == 'background':
 					bground = key_tag.nextSibling.nextSibling.firstChild.data.strip()
 				elif key_tag.firstChild.data.strip() == 'foreground':
 					fground = key_tag.nextSibling.nextSibling.firstChild.data.strip()
+				elif key_tag.firstChild.data.strip() == 'gutterForeground':
+					gfground = key_tag.nextSibling.nextSibling.firstChild.data.strip()
 			except:
 				pass
 		dict_items = the_array.getElementsByTagName('dict')[1:]
@@ -45,29 +47,45 @@ class PrintHtmlCommand(sublime_plugin.TextCommand):
 
 		curr_view = self.view
 		curr_file = curr_view.file_name()
-		head, tail = path.split(curr_file)
-		fname, ext = path.splitext(tail)
-		ext = ext.replace('.', '_')
+		if curr_file is None:
+			fname = 'temp'
+		else:
+			head, tail = path.split(curr_file)
+			fname, ext = path.splitext(tail)
+			fname = fname + ext.replace('.', '_')
+			fname = head + path.sep + fname
 		
 		curr_sel = curr_view.sel()[0]
 		if curr_sel.empty() or abs(curr_sel.end() - curr_sel.begin()) < 4:
-			the_html = open(head + path.sep + fname + ext + '_parsed.html', 'w')
+			the_html = open(fname + '_parsed.html', 'w')
 			size = curr_view.size()
 			pt = 0; end = 1
+			partial = False
 		else:
-			the_html = open(head + path.sep + fname + ext + '_part.html', 'w')
+			the_html = open(fname + '_part.html', 'w')
 			size = curr_sel.end()
 			pt = curr_sel.begin()
 			end = pt + 1
-		the_html.write('<html>\n<head>\n<style type=\"text/css\">\n')
+			curr_row = curr_view.rowcol(pt)[0] + 1
+			partial = True
+		the_html.write('<!DOCTYPE html>\n')
+		the_html.write('<html>\n<head>\n<title>' + fname + '</title>\n')
+		the_html.write('<style type=\"text/css\">\n')
 		the_html.write('\tspan { display: inline; border: 0; margin: 0; padding: 0; }\n')
+		if numbers and gfground != '':
+			the_html.write('\tli { color: ' + gfground  + '; }\n')
 		the_html.write('\tbody { ')
 		if fground != '': the_html.write('color: ' + fground + ';')
 		if bground != '': the_html.write(' background-color: ' + bground + ';')
 		the_html.write(' font: ' + `font_size` + 'pt \"' + font_face + '\", Consolas, Monospace;')
 		the_html.write('\n}\n')
 		the_html.write('</style>\n</head>\n<body>\n')
-		
+		if numbers:
+			if partial:
+				# use code's line numbering
+				the_html.write('<ol>\n<li value="%d">' % curr_row)
+			else:
+				the_html.write('<ol>\n<li>')
 		while end <= size:
 			scope_name = curr_view.scope_name(pt)
 			while curr_view.scope_name(end) == scope_name and end <= size:
@@ -93,11 +111,18 @@ class PrintHtmlCommand(sublime_plugin.TextCommand):
 			tidied_text = tidied_text.replace('>', '&gt;')
 			tidied_text = tidied_text.replace('\t', '&nbsp;' * tab_size)
 			tidied_text = tidied_text.replace(' ' * tab_size, '&nbsp;' * tab_size)
-			tidied_text = tidied_text.replace('\n', '<br>')
-
+			if numbers:
+				new_li = '</span></li>\n<li><span style=\"color:' + the_colour + '\">'
+				tidied_text = tidied_text.replace('\n', new_li)
+			else:
+				tidied_text = tidied_text.replace('\n', '<br>')
+			# for x, y in zip(('&', '<', '>', '\t', ' ' * tab_size, '\n'),
+			# 		('&amp;', '&lt;', '&gt;', '&nbsp;' * tab_size, '&nbsp;' * tab_size, '<br>')):
+			# 	tidied_text = tidied_text.replace(x, y)
 			the_html.write('<span style=\"color:' + the_colour + '\">')
 			the_html.write(tidied_text + '</span>')
 			pt = end
 			end = pt + 1
-		the_html.write('</body>\n</html>')
+		if numbers: the_html.write('</li>\n</ol>')
+		the_html.write('\n</body>\n</html>')
 		the_html.close()
