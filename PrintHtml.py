7151ac73141b4ca88bcf0e4b930cc988c5cbf8df
facelessuser <faceless.shop@gmail.com>
2012-04-18 16:33:16 -0600

    -Merge in Annotations feature -Merge in Format to Forum Post feature -Merge in caret color fix for Print-Color.tmTheme

--- PrintHtml.py
+++ PrintHtml.py
@@ -5,6 +5,8 @@ import tempfile
 import sys
 import datetime
 import webbrowser
+from HtmlAnnotations import get_annotations
+import PrintHtmlLib.desktop as desktop
 
 PACKAGE_SETTINGS = "PrintHtml.sublime-settings"
 
@@ -14,8 +16,6 @@ if sublime.platform() == "linux":
     if not linux_lib in sys.path and path.exists(linux_lib):
         sys.path.append(linux_lib)
 from plistlib import readPlist
-
-import PrintHtmlLib.desktop as desktop
 
 # HTML Code
 CSS = \
@@ -43,15 +43,60 @@ CSS = \
     .code_page { background-color: %(page_bg)s; }
     .code_gutter { background-color: %(gutter_bg)s; }
     .code_line { padding-left: 10px; }
+    span { border: 0; margin: 0; padding: 0; }
     span.bold { font-weight: bold; }
     span.italic { font-style: italic; }
     span.normal { font-style: normal; }
     span.underline { text-decoration:underline; }
-    span { border: 0; margin: 0; padding: 0; }
     body { color: %(body_fg)s; }
+    %(annotations)s
 </style>
 </head>
 """
+
+CSS_ANNOTATIONS = \
+"""
+    .tooltip {
+        border-bottom: 1px dotted %(dot_colour)s;
+        outline: none;
+        text-decoration: none;
+        position: relative;
+    }
+    .tooltip div.annotation {
+        border-radius: 5px 5px;
+        -moz-border-radius: 5px;
+        -webkit-border-radius: 5px;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.1);
+        -webkit-box-shadow: 5px 5px rgba(0, 0, 0, 0.1);
+        -moz-box-shadow: 5px 5px rgba(0, 0, 0, 0.1);
+        white-space: -moz-pre-wrap; /* Mozilla */
+        white-space: -hp-pre-wrap; /* HP printers */
+        white-space: -o-pre-wrap; /* Opera 7 */
+        white-space: -pre-wrap; /* Opera 4-6 */
+        white-space: pre-wrap; /* CSS 2.1 */
+        white-space: pre-line; /* CSS 3 (and 2.1 as well, actually) */
+        word-wrap: break-word; /* IE */
+        margin-left: -999em;
+        position: absolute;
+        padding: 0.8em 1em;
+        background: #FFFFAA; border: 1px solid #FFAD33;
+        font-family: Calibri, Tahoma, Geneva, sans-serif;
+        font-size: 10pt;
+        font-weight: bold;
+        width: 250px;
+        left: 1em;
+        top: 2em;
+        z-index: 99;
+    }
+    .tooltip:hover div.annotation {
+        margin-left: 0;
+    }
+    * html a:hover { background: transparent; }
+"""
+
+ANNOTATE_OPEN = """<a class="tooltip" href="javascript:void(0)">%(code)s"""
+
+ANNOTATE_CLOSE = """<div class="annotation">%(comment)s</div></a>"""
 
 BODY_START = """<body class="code_page code_text">\n<pre class="code_page">"""
 
@@ -80,7 +125,7 @@ ROW_START = """<tr><td>"""
 
 ROW_END = """</td></tr>"""
 
-DIVIDER = """<tr><td><span style="color: %(color)s">\n...\n\n</span></td></tr>"""
+DIVIDER = """<span style="color: %(color)s">\n...\n\n</span>"""
 
 BODY_END = """</pre>\n%(js)s\n</body>\n</html>\n"""
 
@@ -244,8 +289,11 @@ class PrintHtml(object):
         self.pt = 0
         self.end = 0
         self.curr_row = 0
-        self.partial = False
         self.tables = 0
+        self.curr_annot = None
+        self.curr_comment = None
+        self.annotations = self.get_annotations()
+        self.open_annot = False
         self.no_header = no_header
 
         # Get color scheme
@@ -300,13 +348,11 @@ class PrintHtml(object):
             self.pt = 0
             self.end = 1
             self.curr_row = 1
-            self.partial = False
         else:
             self.size = curr_sel.end()
             self.pt = curr_sel.begin()
             self.end = self.pt + 1
             self.curr_row = self.view.rowcol(self.pt)[0] + 1
-            self.partial = True
         self.start_line = self.curr_row
 
         self.gutter_pad = len(str(self.view.rowcol(self.size)[0])) + 1
@@ -355,7 +401,8 @@ class PrintHtml(object):
             "page_bg":   self.bground,
             "gutter_bg": self.gbground,
             "body_fg":   self.fground,
-            "display_mode": 'table-cell' if self.numbers else 'none'
+            "display_mode": 'table-cell' if self.numbers else 'none',
+            "annotations": (CSS_ANNOTATIONS % {"dot_colour": self.fground})
         }
         the_html.write(header)
 
@@ -378,6 +425,76 @@ class PrintHtml(object):
         }
 
         return ''.join(encode_table.get(c, c) for c in text).encode('ascii', 'xmlcharrefreplace')
+
+    def get_annotations(self):
+        annotations = get_annotations(self.view)
+        comments = []
+        for x in range(0, annotations["count"]):
+            region = annotations["annotations"]["html_annotation_%d" % x]["region"]
+            comments.append((region, annotations["annotations"]["html_annotation_%d" % x]["comment"]))
+        comments.sort()
+        return comments
+
+    def annotate_text(self, line, the_colour, the_style, highlight=False):
+        pre_text = None
+        annot_text = None
+        post_text = None
+        start = None
+
+        # Pretext Check
+        if self.pt >= self.curr_annot.begin():
+            # Region starts with an annotation
+            start = self.pt
+        else:
+            # Region has text before annoation
+            pre_text = self.html_encode(self.view.substr(sublime.Region(self.pt, self.curr_annot.begin())))
+            start = self.curr_annot.begin()
+
+        if self.end == self.curr_annot.end():
+            # Region ends annotation
+            annot_text = self.html_encode(self.view.substr(sublime.Region(start, self.end)))
+            self.curr_annot = None
+        elif self.end > self.curr_annot.end():
+            # Region has text following annotation
+            annot_text = self.html_encode(self.view.substr(sublime.Region(start, self.curr_annot.end())))
+            post_text = self.html_encode(self.view.substr(sublime.Region(self.curr_annot.end(), self.end)))
+            self.curr_annot = None
+        else:
+            # Region ends but annotation is not finished
+            annot_text = self.html_encode(self.view.substr(sublime.Region(start, self.end)))
+            self.curr_annot = sublime.Region(self.end, self.curr_annot.end())
+
+        # Print the separate parts pre text, annotation, post text
+        if pre_text != None:
+            self.format_text(line, pre_text, the_colour, the_style, highlight=highlight)
+        if annot_text != None:
+            self.format_text(line, annot_text, the_colour, the_style, highlight=highlight, annotate=True)
+            if self.curr_annot == None:
+                self.curr_comment = None
+        if post_text != None:
+            self.format_text(line, post_text, the_colour, the_style, highlight=highlight)
+
+    def format_text(self, line, text, the_colour, the_style, highlight=False, annotate=False):
+        if highlight:
+            # Highlighted code
+            code = HIGHLIGHTED_CODE % {"highlight": self.sbground, "color": the_colour, "content": text, "class": the_style}
+        else:
+            # Normal code
+            code = CODE % {"color": the_colour, "content": text, "class": the_style}
+        if annotate:
+            if self.curr_annot != None and not self.open_annot:
+                # Open an annotation
+                code = ANNOTATE_OPEN % {"code": code}
+                self.open_annot = True
+            elif self.curr_annot == None:
+                if self.open_annot:
+                    # Close an annotation
+                    code += ANNOTATE_CLOSE % {"comment": self.curr_comment}
+                    self.open_annot = False
+                else:
+                    # Do a complete annotation
+                    code = ANNOTATE_OPEN % {"code": code} + ANNOTATE_CLOSE % {"comment": self.curr_comment}
+        line.append(code)
 
     def convert_line_to_html(self, the_html):
         line = []
@@ -414,18 +531,42 @@ class PrintHtml(object):
                     self.end += 1
                 the_colour, the_style = self.guess_colour(scope_name)
 
-            tidied_text = self.html_encode(self.view.substr(sublime.Region(self.pt, self.end)))
+            # Get new annotation
+            if self.curr_annot == None and len(self.annotations):
+                self.curr_annot, self.curr_comment = self.annotations.pop(0)
+                while self.pt > self.curr_annot[1]:
+                    if len(self.annotations):
+                        self.curr_annot, self.curr_comment = self.annotations.pop(0)
+                    else:
+                        self.curr_annot = None
+                        self.curr_comment = None
+                        break
+                self.curr_annot = sublime.Region(self.curr_annot[0], self.curr_annot[1])
 
-            # Highlight span if needed
+            region = sublime.Region(self.pt, self.end)
+            if self.curr_annot != None and region.intersects(self.curr_annot):
+                # Apply annotation within the text and format the text
+                self.annotate_text(line, the_colour, the_style, highlight=hl_found)
+            else:
+                # Normal text formatting
+                tidied_text = self.html_encode(self.view.substr(region))
+                self.format_text(line, tidied_text, the_colour, the_style, highlight=hl_found)
+
             if hl_found:
-                line.append(HIGHLIGHTED_CODE % {"highlight": self.sbground, "color": self.sfground, "content": tidied_text, "class": the_style})
+                # Clear highlight flags and variables
                 hl_found = False
                 self.curr_hl = None
-            else:
-                line.append(CODE % {"color": the_colour, "content": tidied_text, "class": the_style})
 
+            # Continue walking through line
             self.pt = self.end
             self.end = self.pt + 1
+
+        # Close annotation if open at end of line
+        if self.open_annot:
+            line.append(ANNOTATE_CLOSE % {"comment": self.curr_comment})
+            self.open_annot = False
+
+        # Join line segments
         return ''.join(line)
 
     def write_body(self, the_html):
