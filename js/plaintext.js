18305e46acdb09679dce7069f58c6076c2229cc8
facelessuser <faceless.shop@gmail.com>
2012-11-01 21:56:00 -0600

    -Do not show annotations in plain text

--- js/plaintext.js
+++ js/plaintext.js
@@ -4,16 +4,22 @@
 function toggle_plain_text() {
     var lines = document.querySelectorAll("td.code_line"),
         line_len = lines.length,
-        i, text = "", pre,
+        text = "",
         orig_pre = document.querySelectorAll("pre.code_page")[0],
-        plain_pre = document.querySelectorAll("pre.simple_code_page");
+        plain_pre = document.querySelectorAll("pre.simple_code_page"),
+        pre, i, j, spans, span_len;
     if (plain_pre.length > 0) {
         document.body.removeChild(plain_pre[0]);
         orig_pre.style.display = 'block';
         document.body.className = "code_page";
     } else {
-        for (i = 1; i < line_len; i++) {
-            text += lines[i].textContent;
+        for (i = 0; i < line_len; i++) {
+            spans = lines[i].querySelectorAll("span");
+            span_len = spans.length;
+            for (j = 0; j < span_len; j++) {
+                text += spans[j].textContent;
+            }
+            text += "\n";
         }
         pre = document.createElement('pre');
         pre.className = "simple_code_page";
